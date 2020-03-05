085fed911657b897b2c29c692e74fc4da2cc8d74
jogilsang <jogilsang@gmail.com>
2020-03-05 15:11:32 +0900

    fcm added

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var apisRouter = require('./routes/apis');
+var apisRouter = require('./routes/api');
+var fcmRouter = require('./routes/fcm');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api', apisRouter);
+app.use('/fcm', fcmRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/fcm.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+
+var FCM = require('fcm-node');
+var serverKey = 'AAAAsHK3zM4:APA91bETD6WbzHmslSyMIhiWTqzlOXxCZhheGMhtLSFTyFupQOnp8cEc9PDGjngdina8ZeE_N2ryZoLY5DSPw4LcmfzSMAE0f40S1s2PgcfxzfnHa5eXrtlP_5fs3Q8EKthByvQy-EJ-'; //put your server key here
+var fcm = new FCM(serverKey);
+
+/* localhost:3000 주소로 접속 시 작동되는 라우터 */
+router.get('/', function (req, res, next) {
+  res.send('FCM 단체전송');
+
+  var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+    to: '/topics/default',
+    // collapse_key: 'your_collapse_key',
+
+    notification: {
+      title: 'test title',
+      body: 'test body'
+    }
+
+    // data: {  //you can send only notification or only data(or include both)
+    //   my_key: 'my value',
+    //   my_another_key: 'my another value'
+    // }
+  };
+
+  console.log * (message);
+
+  fcm.send(message, function (err, response) {
+    if (err) {
+      console.log("Something has gone wrong!");
+    } else {
+      console.log("Successfully sent with response: ", response);
+    }
+  });
+
+  // fcm.subscribeToTopic(['device_token_1', 'device_token_2'], 'some_topic_name', (err, res) => {
+  //   assert.ifError(err);
+  //   assert.ok(res);
+  //   done();
+  // });
+
+});
+
+module.exports = router;
+
+
