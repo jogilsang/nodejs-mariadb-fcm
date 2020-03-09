8671316962332a0832417e28bd55fc8ff8a6bbbb
cho_gilsang <cho_gilsang@elandsystems.com>
2020-03-10 08:52:35 +0900

    CORS 패치

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var apisRouter = require('./routes/api');
 var fcmRouter = require('./routes/fcm');
 
 var app = express();
+
+// CORS 설정
+app.use(cors());
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+});
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
