939178b4d914d8f2ef5142b878749e877f315755
jogilsang <jogilsang@gmail.com>
2020-05-07 03:26:47 +0900

    fix : https code moved, from app.js to www

--- app.js
+++ app.js
@@ -17,28 +17,6 @@ var path = require('path')
 
 var app = express();
 
-// https, letsencrypt
-// for ubuntu 18.04
-const fs = require('fs');
-// const path = require('path');
-const HTTPS = require('https');
-var domain = 'jogilsang.xyz' // 구매한 도메인을 기재한다.
-
-try {
-  const option = {
-    ca: fs.readFileSync('/etc/letsencrypt/live/' + domain+ '/fullchain.pem'),
-    key: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/' + domain+ '/privkey.pem'), 'utf8').toString(),
-    cert: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/' + domain+ '/cert.pem'), 'utf8').toString(),
-  };
-
-  HTTPS.createServer(option, app).listen(sslport, () => {
-    console.log('[HTTPS] Soda Server is started on port ${colors.cyan(sslport)}');
-  });
-} catch (error) {
-  console.log('[HTTPS] HTTPS 오류가 발생하였습니다. HTTPS 서버는 실행되지 않습니다.');
-  console.log(error);
-}
-
 // favicon 설정
 app.use(favicon(path.join('./public/images','favicon.ico')));
 
