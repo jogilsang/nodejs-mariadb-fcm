be6e0f6a366fa8dfa35957a48f494aa17f9a6ce2
cho_gilsang <CHO_GILSANG@elandsystems.com>
2020-05-07 20:58:02 +0000

    fix : CORS listen deleted

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ var app = express();
 app.use(favicon(path.join('./public/images','favicon.ico')));
 
 // CORS 설정
-//app.use(cors());
+app.use(cors());
 
-//app.get('/products/:id', function (req, res, next) {
-//  res.json({msg: 'This is CORS-enabled for all origins!'})
-//});
+app.get('/corstest', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+});
 
 //app.listen(80, function () {
 //  console.log('CORS-enabled web server listening on port 80')
